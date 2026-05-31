30311ad970d66ff03d8ca0f02575f956ad74f493
Roberto Ricci <io@r-ricci.it>
2026-05-31 18:29:50 +0200

    update

--- carburanti/assets/p-CBzELu-H-legacy-BcsvvhY5.js
+++ carburanti/assets/p-CBzELu-H-legacy-BcsvvhY5.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-uJd042TC.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.r,n=t.x}],execute:function(){
+System.register(["./index-legacy-6t1YNUxu.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.r,n=t.x}],execute:function(){
 /*!
 			 * (C) Ionic http://ionicframework.com - MIT License
 			 */
