49c15b64dbdc758944208d221c10fb6a23d9ed70
Roberto Ricci <io@r-ricci.it>
2026-05-31 18:35:21 +0200

    Revert "update"
    This reverts commit 30311ad970d66ff03d8ca0f02575f956ad74f493.

--- carburanti/assets/p-CU1SSH8_-legacy-DEoDole7.js
+++ carburanti/assets/p-CU1SSH8_-legacy-DEoDole7.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-6t1YNUxu.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.x,i=e.r}],execute:function(){
+System.register(["./index-legacy-uJd042TC.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.x,i=e.r}],execute:function(){
 /*!
 			 * (C) Ionic http://ionicframework.com - MIT License
 			 */
