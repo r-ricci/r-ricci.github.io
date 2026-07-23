4a594cf0fca690bd48f2d28925c6007b6f201774
Roberto Ricci <io@r-ricci.it>
2026-07-23 17:12:56 +0200

    update

--- carburanti/assets/p-C1aScSTo-legacy-JKwimYqG.js
+++ carburanti/assets/p-C1aScSTo-legacy-JKwimYqG.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-2YhZaWGR.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.r,n=t.B}],execute:function(){
+System.register(["./index-legacy-Bf-bw9lv.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.r,n=t.B}],execute:function(){
 /*!
 			 * (C) Ionic http://ionicframework.com - MIT License
 			 */
--- carburanti/assets/p-D5Ubpm7D-legacy-CZgU8ZAN.js
+++ carburanti/assets/p-D5Ubpm7D-legacy-CZgU8ZAN.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-2YhZaWGR.js"],function(e,t){"use strict";var o,n,i,r,s,a,d,l,c,u;return{setters:[e=>{o=e.a,n=e.t,i=e.l,r=e.e,s=e.m,a=e.u,d=e.i,l=e.d,c=e.f,u=e.g}],execute:function(){
+System.register(["./index-legacy-Bf-bw9lv.js"],function(e,t){"use strict";var o,n,i,r,s,a,d,l,c,u;return{setters:[e=>{o=e.a,n=e.t,i=e.l,r=e.e,s=e.m,a=e.u,d=e.i,l=e.d,c=e.f,u=e.g}],execute:function(){
 /*!
 			 * (C) Ionic http://ionicframework.com - MIT License
 			 */
