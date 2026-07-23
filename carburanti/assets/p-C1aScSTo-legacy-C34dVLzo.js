4435fe6dc1e83ff6ce864ba6f186fa022f323bff
Roberto Ricci <io@r-ricci.it>
2026-07-23 17:05:09 +0200

    update

--- carburanti/assets/p-C1aScSTo-legacy-C34dVLzo.js
+++ carburanti/assets/p-C1aScSTo-legacy-C34dVLzo.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Dfhd3TdO.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.r,n=t.B}],execute:function(){
+System.register(["./index-legacy-2YhZaWGR.js"],function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.r,n=t.B}],execute:function(){
 /*!
 			 * (C) Ionic http://ionicframework.com - MIT License
 			 */
--- carburanti/assets/p-D7bSXQD3-legacy-BkNgSwbd.js
+++ carburanti/assets/p-D7bSXQD3-legacy-BkNgSwbd.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Dfhd3TdO.js"],function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.H,r=e.m,s=e.e,o=e.P,i=e.c}],execute:function(){
+System.register(["./index-legacy-2YhZaWGR.js"],function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.H,r=e.m,s=e.e,o=e.P,i=e.c}],execute:function(){
 /*!
 			 * (C) Ionic http://ionicframework.com - MIT License
 			 */
