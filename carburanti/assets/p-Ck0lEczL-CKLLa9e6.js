04ca5093cf4066c75bb93028078255f0bd861530
Roberto Ricci <io@r-ricci.it>
2026-08-29 18:59:01 +0200

    update

--- carburanti/assets/p-Ck0lEczL-CKLLa9e6.js
+++
@@ -1,3 +0,0 @@
-import{H as r,l as s,t as i,P as a,c}from"./index-CaCGCYoq.js";/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */const m=()=>{const e=window;e.addEventListener("statusTap",()=>{r(()=>{const n=document.elementFromPoint(e.innerWidth/2,e.innerHeight/2);if(!n)return;const t=s(n);t&&new Promise(o=>i(t,o)).then(()=>{a(async()=>{t.style.setProperty("--overflow","hidden"),await c(t,300),t.style.removeProperty("--overflow")})})})})};export{m as startStatusTap};
--- carburanti/registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/carburanti/sw.js', { scope: '/carburanti/' })})}
